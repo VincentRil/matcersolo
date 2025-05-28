47f524d335b0e0d0e6c6c02d490059bcd7a4ca2d
Vincent <pingszz47@gmail.com>
2025-05-28 09:00:58 +0800

    final

--- app/src/main/java/com/example/matematika_cer/network/ApiClient.kt
+++ app/src/main/java/com/example/matematika_cer/network/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://192.168.59.42:8080/api/"
+    private const val BASE_URL = "http://192.168.59.217:8080/api/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
